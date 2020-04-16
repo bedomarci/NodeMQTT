dd679b602bceb9af26ce9f557c7a24918e4802f7
bedomarci <bedo.marton@wst.hu>
2020-04-16 13:03:43 +0200

    adding timestamp build flag

--- scripts/pio_set_flags.py
+++ scripts/pio_set_flags.py
@@ -1,10 +1,14 @@
 Import("env")
+import time
+
 env.Append(CPPDEFINES=[
     ("MQTT_MAX_PACKET_SIZE", 1024),
+    ("UNIX_TIMESTAMP", int(time.time())),
     "_TASK_STD_FUNCTION",
 ])
 global_env = DefaultEnvironment()
 global_env.Append(CPPDEFINES=[
     ("MQTT_MAX_PACKET_SIZE", 1024),
+    ("UNIX_TIMESTAMP", int(time.time())),
     "_TASK_STD_FUNCTION",
 ])
