2c47377ec8e3b51f44496fbf8521e383ab46ad09
bedomarci <bedo.marton@wst.hu>
2021-01-15 12:43:05 +0100

    .

--- src/interfaces/PWMInterface.hpp
+++ src/interfaces/PWMInterface.hpp
@@ -1,18 +1,24 @@
 #ifndef PWMINTERFACE_H
 #define PWMINTERFACE_H
+
 #include "DataInterface.hpp"
 #include "../constants.hpp"
 
-class PWMInterface : public DataInterface<int>
-{
-  public:
+class PWMInterface : public DataInterface<int> {
+public:
     PWMInterface(String topic, uint8_t pwmPin, bool invert = false);
+
     void write(int newValue, bool publishable = true);
+
     void fade(uint16_t fadeFrom, uint16_t fadeTo, uint16_t duration, uint16_t delayed = 0);
+
+    void fadeTo(uint16_t fadeTo, uint16_t duration, uint16_t delayed = 0);
+
     void init();
+
     bool isFading();
 
-  protected:
+protected:
 #if defined(ARDUINO_ARCH_ESP32)
     static uint8_t _PWMChannelCounter;
     uint8_t _currentPWMChannelCounter;
@@ -25,14 +31,16 @@ class PWMInterface : public DataInterface<int>
     uint16_t _fadeIterations;
 
     void updatePhisicalInterface(int newValue);
+
     void pwmWrite(uint8_t pin, int value);
+
     void fadeCallback();
+
     Task tFade;
 };
 
 inline PWMInterface::PWMInterface(String topic, uint8_t pwmPin, bool invert)
-    : DataInterface<int>(topic)
-{
+        : DataInterface<int>(topic) {
     _invert = invert;
     _pwmPin = pwmPin;
     _interfaceName = PWM_NAME;
@@ -48,19 +56,15 @@ inline PWMInterface::PWMInterface(String topic, uint8_t pwmPin, bool invert)
 #endif
 }
 
-inline void PWMInterface::updatePhisicalInterface(int newValue)
-{
-    if (_invert)
-    {
+inline void PWMInterface::updatePhisicalInterface(int newValue) {
+    if (_invert) {
         this->pwmWrite(_pwmPin, PWMRANGE - (constrain(newValue, 0, PWMRANGE)));
-    }
-    else
-    {
+    } else {
         this->pwmWrite(_pwmPin, constrain(newValue, 0, PWMRANGE));
     }
 }
-inline void PWMInterface::fade(uint16_t fadeFrom, uint16_t fadeTo, uint16_t duration, uint16_t delayed)
-{
+
+inline void PWMInterface::fade(uint16_t fadeFrom, uint16_t fadeTo, uint16_t duration, uint16_t delayed) {
     _fadeFrom = constrain(fadeFrom, 0, PWMRANGE);
     _fadeTo = constrain(fadeTo, 0, PWMRANGE);
     _fadeIterations = (duration / (DEFAULT_FADE_INTERVAL));
@@ -68,35 +72,37 @@ inline void PWMInterface::fade(uint16_t fadeFrom, uint16_t fadeTo, uint16_t dura
     tFade.setIterations(_fadeIterations);
     tFade.restartDelayed(delayed);
 }
-inline void PWMInterface::fadeCallback()
-{
+
+inline void PWMInterface::fadeTo(uint16_t fadeTo, uint16_t duration, uint16_t delayed) {
+    this->fade(this->read(), fadeTo, duration, delayed);
+}
+
+inline void PWMInterface::fadeCallback() {
     bool publish = false;
     int value = map(tFade.getRunCounter(), 1, _fadeIterations, _fadeFrom, _fadeTo);
-    if (tFade.isLastIteration()){
+    if (tFade.isLastIteration()) {
         publish = true;
         value = _fadeTo;
     }
     NodeInterface<int>::write(value, publish);
 }
-inline void PWMInterface::write(int newValue, bool publishable)
-{
+
+inline void PWMInterface::write(int newValue, bool publishable) {
     //Writing to the interface disables fade task.
     if (tFade.isEnabled())
         tFade.disable();
     NodeInterface<int>::write(newValue, publishable);
 }
-inline void PWMInterface::init()
-{
+
+inline void PWMInterface::init() {
     getScheduler()->addTask(tFade);
 }
 
-inline bool PWMInterface::isFading()
-{
+inline bool PWMInterface::isFading() {
     return tFade.isEnabled();
 }
 
-inline void PWMInterface::pwmWrite(uint8_t pin, int value)
-{
+inline void PWMInterface::pwmWrite(uint8_t pin, int value) {
 #if defined(ARDUINO_ARCH_ESP32)
     ledcWrite(_currentPWMChannelCounter, value);
 #else
